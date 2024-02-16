782e507f1384cc2cae61caeba7791a4f032b36d6
Anna Valdi <avaldiviesom2122@politecnics.barcelona>
2024-02-16 15:14:36 +0100

    Prototip

--- Admin.Designer.cs
+++ Admin.Designer.cs
@@ -33,17 +33,32 @@
             this.buttonFilter = new System.Windows.Forms.Button();
             this.buttonSort = new System.Windows.Forms.Button();
             this.groupBoxPath = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dateTimePickerFilter = new System.Windows.Forms.DateTimePicker();
+            this.buttonDate = new System.Windows.Forms.Button();
+            this.buttonType = new System.Windows.Forms.Button();
+            this.groupBoxType = new System.Windows.Forms.GroupBox();
+            this.radioButtonType = new System.Windows.Forms.RadioButton();
+            this.radioButtonName = new System.Windows.Forms.RadioButton();
+            this.buttonUpDown = new System.Windows.Forms.Button();
+            this.radioButtonDate = new System.Windows.Forms.RadioButton();
+            this.checkBoxFile = new System.Windows.Forms.CheckBox();
+            this.checkBoxDir = new System.Windows.Forms.CheckBox();
+            this.listBoxPath = new System.Windows.Forms.ListBox();
             this.groupBoxEdit = new System.Windows.Forms.GroupBox();
+            this.textBoxCreate = new System.Windows.Forms.TextBox();
+            this.buttonCreateFile = new System.Windows.Forms.Button();
+            this.buttonCreateDirectory = new System.Windows.Forms.Button();
             this.buttonDelete = new System.Windows.Forms.Button();
             this.buttonModify = new System.Windows.Forms.Button();
-            this.buttonCreate = new System.Windows.Forms.Button();
-            this.buttonCreateDirectory = new System.Windows.Forms.Button();
-            this.buttonCreateFile = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxJSON = new System.Windows.Forms.GroupBox();
+            this.textBoxJSON = new System.Windows.Forms.TextBox();
+            this.buttonCreateJSON = new System.Windows.Forms.Button();
             this.groupBoxPath.SuspendLayout();
+            this.groupBoxType.SuspendLayout();
             this.groupBoxEdit.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBoxJSON.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxPath
@@ -57,7 +72,7 @@
             // 
             this.buttonSelect.Location = new System.Drawing.Point(341, 20);
             this.buttonSelect.Name = "buttonSelect";
-            this.buttonSelect.Size = new System.Drawing.Size(109, 19);
+            this.buttonSelect.Size = new System.Drawing.Size(85, 19);
             this.buttonSelect.TabIndex = 2;
             this.buttonSelect.Text = "Select";
             this.buttonSelect.UseVisualStyleBackColor = true;
@@ -65,9 +80,9 @@
             // 
             // buttonFilter
             // 
-            this.buttonFilter.Location = new System.Drawing.Point(499, 19);
+            this.buttonFilter.Location = new System.Drawing.Point(453, 20);
             this.buttonFilter.Name = "buttonFilter";
-            this.buttonFilter.Size = new System.Drawing.Size(109, 19);
+            this.buttonFilter.Size = new System.Drawing.Size(99, 19);
             this.buttonFilter.TabIndex = 3;
             this.buttonFilter.Text = "Filter";
             this.buttonFilter.UseVisualStyleBackColor = true;
@@ -75,7 +90,7 @@
             // 
             // buttonSort
             // 
-            this.buttonSort.Location = new System.Drawing.Point(614, 19);
+            this.buttonSort.Location = new System.Drawing.Point(578, 20);
             this.buttonSort.Name = "buttonSort";
             this.buttonSort.Size = new System.Drawing.Size(109, 19);
             this.buttonSort.TabIndex = 4;
@@ -85,100 +100,232 @@
             // 
             // groupBoxPath
             // 
+            this.groupBoxPath.Controls.Add(this.dateTimePickerFilter);
+            this.groupBoxPath.Controls.Add(this.buttonDate);
+            this.groupBoxPath.Controls.Add(this.buttonType);
+            this.groupBoxPath.Controls.Add(this.groupBoxType);
             this.groupBoxPath.Controls.Add(this.buttonSort);
             this.groupBoxPath.Controls.Add(this.buttonFilter);
             this.groupBoxPath.Controls.Add(this.buttonSelect);
             this.groupBoxPath.Controls.Add(this.textBoxPath);
             this.groupBoxPath.Location = new System.Drawing.Point(23, 25);
             this.groupBoxPath.Name = "groupBoxPath";
-            this.groupBoxPath.Size = new System.Drawing.Size(741, 50);
+            this.groupBoxPath.Size = new System.Drawing.Size(701, 149);
             this.groupBoxPath.TabIndex = 5;
             this.groupBoxPath.TabStop = false;
             this.groupBoxPath.Text = "Path";
             // 
-            // listBox1
+            // dateTimePickerFilter
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(37, 38);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(400, 238);
-            this.listBox1.TabIndex = 0;
+            this.dateTimePickerFilter.Location = new System.Drawing.Point(431, 111);
+            this.dateTimePickerFilter.Name = "dateTimePickerFilter";
+            this.dateTimePickerFilter.Size = new System.Drawing.Size(143, 20);
+            this.dateTimePickerFilter.TabIndex = 9;
+            // 
+            // buttonDate
+            // 
+            this.buttonDate.Location = new System.Drawing.Point(471, 84);
+            this.buttonDate.Name = "buttonDate";
+            this.buttonDate.Size = new System.Drawing.Size(65, 21);
+            this.buttonDate.TabIndex = 13;
+            this.buttonDate.Text = "Date";
+            this.buttonDate.UseVisualStyleBackColor = true;
+            this.buttonDate.Click += new System.EventHandler(this.buttonDate_Click);
+            // 
+            // buttonType
+            // 
+            this.buttonType.Location = new System.Drawing.Point(471, 44);
+            this.buttonType.Name = "buttonType";
+            this.buttonType.Size = new System.Drawing.Size(65, 21);
+            this.buttonType.TabIndex = 12;
+            this.buttonType.Text = "Type";
+            this.buttonType.UseVisualStyleBackColor = true;
+            this.buttonType.Click += new System.EventHandler(this.buttonType_Click);
+            // 
+            // groupBoxType
+            // 
+            this.groupBoxType.Controls.Add(this.radioButtonType);
+            this.groupBoxType.Controls.Add(this.radioButtonName);
+            this.groupBoxType.Controls.Add(this.buttonUpDown);
+            this.groupBoxType.Controls.Add(this.radioButtonDate);
+            this.groupBoxType.Controls.Add(this.checkBoxFile);
+            this.groupBoxType.Controls.Add(this.checkBoxDir);
+            this.groupBoxType.Location = new System.Drawing.Point(578, 45);
+            this.groupBoxType.Name = "groupBoxType";
+            this.groupBoxType.Size = new System.Drawing.Size(105, 86);
+            this.groupBoxType.TabIndex = 9;
+            this.groupBoxType.TabStop = false;
+            // 
+            // radioButtonType
+            // 
+            this.radioButtonType.AutoSize = true;
+            this.radioButtonType.Location = new System.Drawing.Point(9, 64);
+            this.radioButtonType.Name = "radioButtonType";
+            this.radioButtonType.Size = new System.Drawing.Size(49, 17);
+            this.radioButtonType.TabIndex = 16;
+            this.radioButtonType.TabStop = true;
+            this.radioButtonType.Text = "Type";
+            this.radioButtonType.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonName
+            // 
+            this.radioButtonName.AutoSize = true;
+            this.radioButtonName.Location = new System.Drawing.Point(9, 17);
+            this.radioButtonName.Name = "radioButtonName";
+            this.radioButtonName.Size = new System.Drawing.Size(53, 17);
+            this.radioButtonName.TabIndex = 14;
+            this.radioButtonName.TabStop = true;
+            this.radioButtonName.Text = "Name";
+            this.radioButtonName.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpDown
+            // 
+            this.buttonUpDown.Location = new System.Drawing.Point(71, 19);
+            this.buttonUpDown.Name = "buttonUpDown";
+            this.buttonUpDown.Size = new System.Drawing.Size(28, 21);
+            this.buttonUpDown.TabIndex = 10;
+            this.buttonUpDown.Text = "↑";
+            this.buttonUpDown.UseVisualStyleBackColor = true;
+            this.buttonUpDown.Click += new System.EventHandler(this.buttonUpDown_Click);
+            // 
+            // radioButtonDate
+            // 
+            this.radioButtonDate.AutoSize = true;
+            this.radioButtonDate.Location = new System.Drawing.Point(9, 40);
+            this.radioButtonDate.Name = "radioButtonDate";
+            this.radioButtonDate.Size = new System.Drawing.Size(48, 17);
+            this.radioButtonDate.TabIndex = 15;
+            this.radioButtonDate.TabStop = true;
+            this.radioButtonDate.Text = "Date";
+            this.radioButtonDate.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFile
+            // 
+            this.checkBoxFile.AutoSize = true;
+            this.checkBoxFile.Location = new System.Drawing.Point(9, 44);
+            this.checkBoxFile.Name = "checkBoxFile";
+            this.checkBoxFile.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxFile.TabIndex = 1;
+            this.checkBoxFile.Text = "Files";
+            this.checkBoxFile.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDir
+            // 
+            this.checkBoxDir.AutoSize = true;
+            this.checkBoxDir.Location = new System.Drawing.Point(9, 19);
+            this.checkBoxDir.Name = "checkBoxDir";
+            this.checkBoxDir.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxDir.TabIndex = 0;
+            this.checkBoxDir.Text = "Directories";
+            this.checkBoxDir.UseVisualStyleBackColor = true;
+            // 
+            // listBoxPath
+            // 
+            this.listBoxPath.FormattingEnabled = true;
+            this.listBoxPath.Location = new System.Drawing.Point(37, 38);
+            this.listBoxPath.Name = "listBoxPath";
+            this.listBoxPath.Size = new System.Drawing.Size(400, 238);
+            this.listBoxPath.TabIndex = 0;
             // 
             // groupBoxEdit
             // 
+            this.groupBoxEdit.Controls.Add(this.textBoxCreate);
             this.groupBoxEdit.Controls.Add(this.buttonCreateFile);
             this.groupBoxEdit.Controls.Add(this.buttonCreateDirectory);
             this.groupBoxEdit.Controls.Add(this.buttonDelete);
             this.groupBoxEdit.Controls.Add(this.buttonModify);
-            this.groupBoxEdit.Controls.Add(this.buttonCreate);
-            this.groupBoxEdit.Location = new System.Drawing.Point(522, 109);
+            this.groupBoxEdit.Location = new System.Drawing.Point(522, 302);
             this.groupBoxEdit.Name = "groupBoxEdit";
-            this.groupBoxEdit.Size = new System.Drawing.Size(242, 312);
+            this.groupBoxEdit.Size = new System.Drawing.Size(202, 175);
             this.groupBoxEdit.TabIndex = 7;
             this.groupBoxEdit.TabStop = false;
             this.groupBoxEdit.Text = "Edit";
             // 
+            // textBoxCreate
+            // 
+            this.textBoxCreate.Location = new System.Drawing.Point(34, 19);
+            this.textBoxCreate.Name = "textBoxCreate";
+            this.textBoxCreate.Size = new System.Drawing.Size(150, 20);
+            this.textBoxCreate.TabIndex = 6;
+            // 
+            // buttonCreateFile
+            // 
+            this.buttonCreateFile.Location = new System.Drawing.Point(34, 45);
+            this.buttonCreateFile.Name = "buttonCreateFile";
+            this.buttonCreateFile.Size = new System.Drawing.Size(150, 23);
+            this.buttonCreateFile.TabIndex = 4;
+            this.buttonCreateFile.Text = "Create file";
+            this.buttonCreateFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreateDirectory
+            // 
+            this.buttonCreateDirectory.Location = new System.Drawing.Point(34, 74);
+            this.buttonCreateDirectory.Name = "buttonCreateDirectory";
+            this.buttonCreateDirectory.Size = new System.Drawing.Size(150, 23);
+            this.buttonCreateDirectory.TabIndex = 3;
+            this.buttonCreateDirectory.Text = "Create directory";
+            this.buttonCreateDirectory.UseVisualStyleBackColor = true;
+            // 
             // buttonDelete
             // 
-            this.buttonDelete.Location = new System.Drawing.Point(42, 194);
+            this.buttonDelete.Location = new System.Drawing.Point(34, 131);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(170, 20);
+            this.buttonDelete.Size = new System.Drawing.Size(150, 20);
             this.buttonDelete.TabIndex = 2;
             this.buttonDelete.Text = "Delete";
             this.buttonDelete.UseVisualStyleBackColor = true;
             // 
             // buttonModify
             // 
-            this.buttonModify.Location = new System.Drawing.Point(42, 121);
+            this.buttonModify.Location = new System.Drawing.Point(34, 103);
             this.buttonModify.Name = "buttonModify";
-            this.buttonModify.Size = new System.Drawing.Size(170, 22);
+            this.buttonModify.Size = new System.Drawing.Size(150, 22);
             this.buttonModify.TabIndex = 1;
             this.buttonModify.Text = "Modify name";
             this.buttonModify.UseVisualStyleBackColor = true;
             // 
-            // buttonCreate
-            // 
-            this.buttonCreate.Location = new System.Drawing.Point(42, 56);
-            this.buttonCreate.Name = "buttonCreate";
-            this.buttonCreate.Size = new System.Drawing.Size(170, 20);
-            this.buttonCreate.TabIndex = 0;
-            this.buttonCreate.Text = "Create";
-            this.buttonCreate.UseVisualStyleBackColor = true;
-            // 
-            // buttonCreateDirectory
-            // 
-            this.buttonCreateDirectory.Location = new System.Drawing.Point(42, 89);
-            this.buttonCreateDirectory.Name = "buttonCreateDirectory";
-            this.buttonCreateDirectory.Size = new System.Drawing.Size(75, 23);
-            this.buttonCreateDirectory.TabIndex = 3;
-            this.buttonCreateDirectory.Text = "Directory";
-            this.buttonCreateDirectory.UseVisualStyleBackColor = true;
-            this.buttonCreateDirectory.Visible = false;
-            // 
-            // buttonCreateFile
-            // 
-            this.buttonCreateFile.Location = new System.Drawing.Point(137, 89);
-            this.buttonCreateFile.Name = "buttonCreateFile";
-            this.buttonCreateFile.Size = new System.Drawing.Size(75, 23);
-            this.buttonCreateFile.TabIndex = 4;
-            this.buttonCreateFile.Text = "File";
-            this.buttonCreateFile.UseVisualStyleBackColor = true;
-            // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Location = new System.Drawing.Point(26, 109);
+            this.groupBox1.Controls.Add(this.listBoxPath);
+            this.groupBox1.Location = new System.Drawing.Point(12, 180);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(478, 311);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBoxJSON
+            // 
+            this.groupBoxJSON.Controls.Add(this.textBoxJSON);
+            this.groupBoxJSON.Controls.Add(this.buttonCreateJSON);
+            this.groupBoxJSON.Location = new System.Drawing.Point(524, 195);
+            this.groupBoxJSON.Name = "groupBoxJSON";
+            this.groupBoxJSON.Size = new System.Drawing.Size(202, 93);
+            this.groupBoxJSON.TabIndex = 8;
+            this.groupBoxJSON.TabStop = false;
+            this.groupBoxJSON.Text = "JSON";
+            // 
+            // textBoxJSON
+            // 
+            this.textBoxJSON.Location = new System.Drawing.Point(32, 19);
+            this.textBoxJSON.Name = "textBoxJSON";
+            this.textBoxJSON.Size = new System.Drawing.Size(150, 20);
+            this.textBoxJSON.TabIndex = 5;
+            // 
+            // buttonCreateJSON
+            // 
+            this.buttonCreateJSON.Location = new System.Drawing.Point(32, 45);
+            this.buttonCreateJSON.Name = "buttonCreateJSON";
+            this.buttonCreateJSON.Size = new System.Drawing.Size(150, 23);
+            this.buttonCreateJSON.TabIndex = 4;
+            this.buttonCreateJSON.Text = "Create JSON";
+            this.buttonCreateJSON.UseVisualStyleBackColor = true;
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 439);
+            this.ClientSize = new System.Drawing.Size(738, 504);
+            this.Controls.Add(this.groupBoxJSON);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBoxEdit);
             this.Controls.Add(this.groupBoxPath);
@@ -186,8 +333,13 @@
             this.Text = "Admin";
             this.groupBoxPath.ResumeLayout(false);
             this.groupBoxPath.PerformLayout();
+            this.groupBoxType.ResumeLayout(false);
+            this.groupBoxType.PerformLayout();
             this.groupBoxEdit.ResumeLayout(false);
+            this.groupBoxEdit.PerformLayout();
             this.groupBox1.ResumeLayout(false);
+            this.groupBoxJSON.ResumeLayout(false);
+            this.groupBoxJSON.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -200,13 +352,26 @@
         private System.Windows.Forms.Button buttonSort;
         private System.Windows.Forms.GroupBox groupBoxPath;
         private System.Windows.Forms.GroupBox groupBoxEdit;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBoxPath;
         private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.Button buttonModify;
-        private System.Windows.Forms.Button buttonCreate;
         private System.Windows.Forms.Button buttonCreateDirectory;
         private System.Windows.Forms.Button buttonCreateFile;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxType;
+        private System.Windows.Forms.CheckBox checkBoxFile;
+        private System.Windows.Forms.CheckBox checkBoxDir;
+        private System.Windows.Forms.Button buttonDate;
+        private System.Windows.Forms.Button buttonType;
+        private System.Windows.Forms.Button buttonUpDown;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFilter;
+        private System.Windows.Forms.RadioButton radioButtonType;
+        private System.Windows.Forms.RadioButton radioButtonName;
+        private System.Windows.Forms.RadioButton radioButtonDate;
+        private System.Windows.Forms.GroupBox groupBoxJSON;
+        private System.Windows.Forms.Button buttonCreateJSON;
+        private System.Windows.Forms.TextBox textBoxJSON;
+        private System.Windows.Forms.TextBox textBoxCreate;
     }
 }
 
--- Admin.cs
+++ Admin.cs
@@ -17,6 +17,20 @@ namespace M06_RepteTry2
         public Admin()
         {
             InitializeComponent();
+
+            //Filter
+            buttonType.Visible = false;
+            groupBoxType.Visible = false;
+            checkBoxDir.Visible = false;   
+            checkBoxFile.Visible = false;
+            buttonDate.Visible = false;
+            dateTimePickerFilter.Visible = false;
+            //Sort No funcionen els botons
+            //radioButtonName.Visible = false;
+            //radioButtonDate.Visible = false;
+            //radioButtonType.Visible = false;
+            buttonUpDown.Visible = false;
+
         }
 
         private void buttonSelect_Click(object sender, EventArgs e)
@@ -28,18 +42,103 @@ namespace M06_RepteTry2
             {
                 textBoxPath.Text = folder.SelectedPath;
                 directoryActual = new DirectoryInfo(textBoxPath.Text);
+
+                // Mostrem els directoris
+                listBoxPath.Items.Clear();
+                foreach (var directory in directoryActual.GetDirectories())
+                {
+                    listBoxPath.Items.Add(directory.Name);
+                }
+
+                // Mostrem els arxius
+                foreach (var file in directoryActual.GetFiles())
+                {
+                    listBoxPath.Items.Add(file.Name);
+                }
             }
         }
         private void buttonFilter_Click(object sender, EventArgs e)
         {
-            Filter filter = new Filter();
-            filter.ShowDialog(); 
+            if (buttonType.Visible == false)
+            {
+                //true filter
+                buttonType.Visible = true;
+                buttonDate.Visible = true;
+                //false sort
+                radioButtonName.Visible = false;
+                radioButtonDate.Visible = false;
+                radioButtonType.Visible = false;
+                groupBoxType.Visible = false;
+                buttonUpDown.Visible = false;
+            }
+            else
+            {
+                buttonType.Visible = false;
+                groupBoxType.Visible = false;
+                checkBoxDir.Visible = false;
+                checkBoxFile.Visible = false;
+                buttonDate.Visible = false;
+                dateTimePickerFilter.Visible = false;
+            }
         }
 
         private void buttonSort_Click(object sender, EventArgs e)
         {
-            Sort sort = new Sort();
-            sort.ShowDialog();
+            if (buttonUpDown.Visible == false)
+            {
+                //true sort
+                radioButtonName.Visible = true;
+                radioButtonDate.Visible = true;
+                radioButtonType.Visible = true;
+                buttonUpDown.Visible = true;
+                groupBoxType.Visible = true;
+                //false filtre
+                buttonType.Visible = false;
+                checkBoxDir.Visible = false;
+                checkBoxFile.Visible = false;
+                buttonDate.Visible = false;
+                dateTimePickerFilter.Visible = false;
+            }
+            else
+            {
+                radioButtonName.Visible = false;
+                radioButtonDate.Visible = false;
+                radioButtonType.Visible = false;
+                buttonUpDown.Visible = false;
+                groupBoxType.Visible = false;
+            }
+        }
+
+        private void buttonType_Click(object sender, EventArgs e)
+        {
+            if (groupBoxType.Visible == false)
+            {
+                groupBoxType.Visible = true;
+                checkBoxDir.Visible = true;
+                checkBoxFile.Visible = true;
+            }
+            else
+            {
+                groupBoxType.Visible = false;
+                checkBoxDir.Visible = false;
+                checkBoxFile.Visible = false;
+            }
+        }
+
+        private void buttonDate_Click(object sender, EventArgs e)
+        {
+            if (dateTimePickerFilter.Visible == false)
+                dateTimePickerFilter.Visible = true;
+            else
+                dateTimePickerFilter.Visible = false;
+        }
+
+        private void buttonUpDown_Click(object sender, EventArgs e)
+        {
+            if (buttonUpDown.Text == "↑")
+                    buttonUpDown.Text = "↓";
+            else
+                buttonUpDown.Text = "↑";
         }
 
     }
